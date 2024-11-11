92687b296f5386f7a4f9049417557968ecb049a1
joanne_cmd <joanne.m.muthoni@gmail.com>
2024-11-11 10:14:58 +0300

    learn to use errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub trait Account{
-    fn deposit(&mut self, amount: f64);
-    fn withdraw(&mut self, amount: f64);
+    fn deposit(&mut self, amount: f64) -> Result<(), String> ;
+    fn withdraw(&mut self, amount: f64) -> Result<(), String>;
     fn balance(&self) -> f64;
 }
 pub struct BankAccount{
@@ -12,12 +12,24 @@ pub struct BankAccount{
 
 impl Account for BankAccount {
 
-    fn deposit(&mut self, amount: f64) {
+    fn deposit(&mut self, amount: f64) -> Result<(), String> {
+        if amount <= 0.0 {
+            // Return an error if the amount is invalid
+            return Err("Please enter a positive deposit amount".to_string());
+        }
+        
+        // Otherwise, add the amount to the balance
         self.balance += amount;
+        Ok(())
     }
+    
 
-    fn withdraw(&mut self, amount: f64) {
+    fn withdraw(&mut self, amount: f64)->  Result<(), String> {
+        if amount<= 0.0{
+            return Err("please top up your account to fill  it".to_string());
+        }
         self.balance -= amount;
+        Ok(())
     }
 
     fn balance(&self) -> f64 {
@@ -38,8 +50,15 @@ impl Account for BankAccount {
          balance: 20000.0,
      };
      
-     bank_account1.deposit(5000.0);
-     bank_account2.withdraw(300.0);
+     match bank_account1.deposit(5000.0){
+        Ok(_)=> println!("Deposit successfull!"),
+        Err(e)=> println!("Failed to deposit: {}", e)
+     };
+     match bank_account2.withdraw(300.0) {
+         Ok(_)=> println!("withdraw successful!"),
+         Err(e)=> println!("insucfienct funds{}!", e)
+
+     };
      
      println!("this it the balance of accout 1: {}",  bank_account1.balance());
      
